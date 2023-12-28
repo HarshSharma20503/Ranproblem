8d4171bd0ab3e6f0edff301900be68f7f1745d0d
Harsh Sharma <harshsharma20503@gmail.com>
2023-12-29 02:28:51 +0530

    added user agents and args in puppeter launch

--- scrape/getLeetcodeQuestions.js
+++ scrape/getLeetcodeQuestions.js
@@ -18,7 +18,14 @@ const getLeetcodeQuestions = async (url) => {
         console.log("got todays date - ", formattedDate);
 
         console.log("launching puppeteer");
-        const browser = await puppeteer.launch({ headless: "new", executablePath: './.cache/puppeteer/chrome/linux-119.0.6045.105/chrome-linux64/chrome' });
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: './.cache/puppeteer/chrome/linux-119.0.6045.105/chrome-linux64/chrome',
+            args: [
+                '--enable-audio',
+                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+            ]
+        });
         const page = await browser.newPage();
         console.log("puppeteer launched");
 
@@ -34,28 +41,30 @@ const getLeetcodeQuestions = async (url) => {
         });
         const $ = cheerio.load(pageData.html);
 
-        // seclect the anchor tag with problem of the day of current date
-        const filteredAnchorTags = $(`a[href^='/problems/'][href*='/?envType=daily-question&envId=${formattedDate}']`);
+        const filteredAnchorTags = $("a[href^='/problems/'][href*='/?envType=daily-question&envId=']");
 
-        // Log the href attribute of each filtered anchor tag
-        const firstHref = filteredAnchorTags.first().attr("href");
-        console.log(`Href of the first element: ${firstHref}`);
+        const linksArray = [];
+        filteredAnchorTags.each((index, element) => {
+            const link = $(element).attr('href');
+            console.log(link);
+            linksArray.push(link);
+        });
 
-        const problemOfTheDayLink = `https://leetcode.com${firstHref}`;
+        const lastLink = linksArray.length > 0 ? linksArray[linksArray.length - 1] : null;
+
+        console.log(lastLink);
+        const problemOfTheDayLink = `https://leetcode.com${lastLink}`;
+        console.log(problemOfTheDayLink);
 
         await browser.close();
         if (problemOfTheDayLink) {
-            // console.log('Problem of the Day Link:', problemOfTheDayLink);
             return problemOfTheDayLink;
         } else {
             console.log('Problem of the Day Link not found.');
             return null;
         }
     } catch (error) {
-        // Handle errors if the request fails
         console.error('Error:', error.message);
-
-        // Re-throw the error to signal that the function has failed
         throw error;
     }
 }
