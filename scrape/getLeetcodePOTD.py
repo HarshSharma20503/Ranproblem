581ff2117e6fdabf29d639eff274d30b157ecd50
Harsh Sharma <harshsharma20503@gmail.com>
2024-01-01 01:48:26 +0530

    removed comments from getLeetcodePOTD

--- scrape/getLeetcodePOTD.py
+++ scrape/getLeetcodePOTD.py
@@ -1,34 +1,31 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
-# def get_problem_links():
-#     try:
-#         url = "https://leetcode.com/problemset/"
+def get_problem_links():
+    try:
+        url = "https://leetcode.com/problemset/"
 
-#         response = requests.get(url)
-#         response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception if the request was unsuccessful
 
-#         soup = BeautifulSoup(response.content, "html.parser")
+        soup = BeautifulSoup(response.content, "html.parser")
 
-#         links = []
-#         for link in soup.find_all("a"):
-#             href = link.get("href")
-#             if href and "/problems/" in href and "/?envType=daily-question&envId=" in href:
-#                 links.append(href)
+        links = []
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if href and "/problems/" in href and "/?envType=daily-question&envId=" in href:
+                links.append(href)
 
-#         return links
+        return links
 
-#     except requests.exceptions.RequestException as e:
-#         print("An error occurred while making the request:", str(e))
-#         return []
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while making the request:", str(e))
+        return []
 
-#     except Exception as e:
-#         print("An error occurred:", str(e))
-#         return []
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return []
 
-# problem_links = get_problem_links()
-# if problem_links:
-#     print(problem_links[-1])  # Print the last problem link
-
-import sys
-print(sys.executable)
+problem_links = get_problem_links()
+if problem_links:
+    print(problem_links[-1])  # Print the last problem link
